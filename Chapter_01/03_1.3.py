f0e566e612a30ef1fb879f23f54fe622687915c9
Natady25 <nata.dwiyanti@gmail.com>
2026-04-15 22:11:51 +0900

    Fix formatting and spacing in Chapter_01/03_1.3.py

--- Chapter_01/03_1.3.py
+++ Chapter_01/03_1.3.py
@@ -1,9 +1,9 @@
-#Versi 1
+# Versi 1
 print("FFFFFFF   U       U   NN     NN")
 print("FF        U       U   NNN    NN")
 print("FFFFFFF   U       U   NN N   NN")
 print("FF          U   U     NN  N  NN")
 print("FF           UUU      N     NNN")
 
-#Versi 2
+# Versi 2
 print("FFFFFFF\tU       U\tNN     NN\nFF     \tU       U\tNNN    NN\nFFFFFFF\tU       U\tNN N   NN\nFF     \t  U   U  \tNN  N  NN\nFF     \t   UUU   \tN     NNN")
