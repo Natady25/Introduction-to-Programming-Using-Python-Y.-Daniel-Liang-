44586419635ff0f6231c495de59fb5edef417860
Natady25 <nata.dwiyanti@gmail.com>
2026-04-15 22:38:48 +0900

    Update 06_1.6.py

--- Chapter_01/06_1.6.py
+++ Chapter_01/06_1.6.py
@@ -1,1 +1,15 @@
+# Versi 1
 print(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9)
+
+# Versi 2
+angka = 1
+total = 0
+
+while angka <= 9:
+  total += angka
+  angka += 1
+
+print(total)
+
+# Versi 3
+print(sum(range(1, 10)))
