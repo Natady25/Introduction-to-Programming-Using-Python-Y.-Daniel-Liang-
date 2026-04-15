a10105ddb7bea8d0373e99d4ec4b3040775e748f
Natady25 <nata.dwiyanti@gmail.com>
2026-04-15 21:59:14 +0900

    Update 02_1.2.py

--- Chapter_01/02_1.2.py
+++ Chapter_01/02_1.2.py
@@ -3,3 +3,13 @@ print("Welcome to Python")
 print("Welcome to Python")
 print("Welcome to Python")
 print("Welcome to Python")
+
+#Versi 2
+print("Welcome to Python\nWelcome to Python\nWelcome to Python\nWelcome to Python\nWelcome to Python")
+
+#Versi 3
+a = 1
+
+while a < 6:
+  print("Welcome to Python")
+  a += 1
