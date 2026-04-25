a5665642cc596dd303a4c2447a58dd41700917ab
Natady25 <nata.dwiyanti@gmail.com>
2026-04-26 00:32:44 +0900

    Update 08_1.8.py

--- Chapter_01/08_1.8.py
+++ Chapter_01/08_1.8.py
@@ -1,5 +1,15 @@
+# Versi 1
 PI = 3.14
 radius = 5.5
 area = radius * radius * PI
 perimeter = 2 * radius * PI
+print("Area of circle =", area, "\nPerimeter of circle =", perimeter)
+
+# Versi 2
+import math
+radius = 5.5
+
+area = radius * radius * math.pi
+perimeter = 2 * radius * math.pi
+
 print("Area of circle =", area, "\nPerimeter of circle =", perimeter)
