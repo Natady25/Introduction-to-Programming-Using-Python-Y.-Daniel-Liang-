8eebe943c9445553a3603944e0abca07294040e3
Natady25 <nata.dwiyanti@gmail.com>
2026-04-29 17:36:25 +0900

    Update 09_1.9.py

--- Chapter_01/09_1.9.py
+++ Chapter_01/09_1.9.py
@@ -13,3 +13,12 @@ area = width * height
 perimeter = 2 * (width + height)
 
 print("Area of rectangle =", area, "\nPerimeter of rectangle =", perimeter)
+
+# Versi 3
+width = 4.5
+height = 7.9
+
+area = width * height
+perimeter = 2 * (width + height)
+
+print(f"Area of rectangle = {area}\nPerimeter of rectangle = {perimeter}")
