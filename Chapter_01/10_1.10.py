3857d04de88a42d24611e20b020658c0ad84c755
Natady25 <nata.dwiyanti@gmail.com>
2026-04-29 18:24:43 +0900

    Refactor and restore code for average speed calculation

--- Chapter_01/10_1.10.py
+++ Chapter_01/10_1.10.py
@@ -1,12 +1,10 @@
 # Versi 1
-
 distance_in_kilometers = 14
 distance_in_miles = 14 * (1 / 1.6)
 time_in_hours = 45.5 * (1 / 60)
 print("Average Speed in Miles per Hour=", distance_in_miles / time_in_hours, "miles per hours")
 
 # Versi 2
-
 kilometers = 14
 minutes = 45
 seconds = 30
