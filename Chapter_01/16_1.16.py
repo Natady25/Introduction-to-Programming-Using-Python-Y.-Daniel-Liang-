cd655bb7642436a947ad0af2ed83e8efcd320769
Natady25 <nata.dwiyanti@gmail.com>
2026-05-03 12:21:29 +0900

    Implement multiple versions of turtle drawing
    Added multiple versions of turtle graphics drawing with different starting positions and loops.

--- Chapter_01/16_1.16.py
+++ Chapter_01/16_1.16.py
@@ -1,3 +1,4 @@
+# Versi 1
 import turtle
 turtle.showturtle()
 
@@ -13,4 +14,33 @@ turtle.pendown()
 turtle.circle(50)
 turtle.right(180)
 turtle.circle(50)
+turtle.done()
+
+# Versi 2
+import turtle
+turtle.showturtle()
+
+turtle.penup()
+turtle.goto(-50,0)
+for i in range(2):
+  turtle.penup()
+  turtle.forward(100)
+  turtle.pendown()
+  for j in range(2):
+    turtle.circle(50)
+    turtle.right(180)
+  turtle.right(180)    
+turtle.done()
+
+# Versi 3
+import turtle
+turtle.showturtle()
+
+posisi_awal = [(-50, 0), (50, 0), (-50, -100), (50, -100)]
+
+for x, y in posisi_awal:
+  turtle.penup()
+  turtle.goto(x, y)
+  turtle.pendown()
+  turtle.circle(50)   
 turtle.done()
