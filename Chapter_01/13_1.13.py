4019bd76fc750a1af1fb352504c2a26c5349b957
Natady25 <nata.dwiyanti@gmail.com>
2026-05-03 10:34:55 +0900

    Update 13_1.13.py

--- Chapter_01/13_1.13.py
+++ Chapter_01/13_1.13.py
@@ -1,3 +1,4 @@
+# Versi 1
 import turtle
 turtle.showturtle()
 
@@ -14,4 +15,20 @@ turtle.forward(50)
 turtle.right(90)
 turtle.pendown()
 turtle.forward(100)
+turtle.done()
+
+# Versi 2
+import turtle
+turtle.showturtle()
+
+turtle.penup()
+turtle.goto(-50,0)
+turtle.pendown()
+turtle.forward(100)
+turtle.penup()
+turtle.goto(0,50)
+turtle.right(90)
+turtle.pendown()
+turtle.forward(100)
+
 turtle.done()
