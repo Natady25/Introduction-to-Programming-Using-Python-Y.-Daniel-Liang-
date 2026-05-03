9987b5f63f19c5829897b8c5cadaf4a90925dd90
Natady25 <nata.dwiyanti@gmail.com>
2026-05-03 13:03:03 +0900

    Update 17_1.17.py

--- Chapter_01/17_1.17.py
+++ Chapter_01/17_1.17.py
@@ -2,9 +2,12 @@ import turtle
 turtle.showturtle()
 
 turtle.penup()
-turtle.goto(-39,49)
-turtle.write("(-39,49)")
+turtle.goto(-39, 48)
+turtle.write("(-39, 48)")
+turtle.pencolor("red")
 turtle.pendown()
-turtle.goto(50,-50)
-turtle.write("(50,-50)")
+turtle.goto(50, -50)
+turtle.pencolor("black")
+turtle.write("(50, -50)")
+
 turtle.done()
