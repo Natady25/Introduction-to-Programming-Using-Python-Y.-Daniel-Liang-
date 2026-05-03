dce8af74a557b625a2c25e321df92c00ae38636d
Natady25 <nata.dwiyanti@gmail.com>
2026-05-03 11:20:20 +0900

    Update 15_1.15.py

--- Chapter_01/15_1.15.py
+++ Chapter_01/15_1.15.py
@@ -1,3 +1,4 @@
+# Versi 1
 import turtle
 turtle.showturtle()
 
@@ -11,4 +12,16 @@ turtle.left(120)
 turtle.forward(100)
 turtle.left(120)
 turtle.forward(100)
+turtle.done()
+
+# Versi 2
+import turtle
+turtle.showturtle()
+
+turtle.right(60)
+for i in range(2):
+  for j in range(3):
+    turtle.forward(100)
+    turtle.right(120)
+  turtle.left(180)
 turtle.done()
