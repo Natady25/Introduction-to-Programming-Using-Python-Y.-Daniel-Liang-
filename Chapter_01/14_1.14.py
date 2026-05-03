ae91f4bb7e881f08d7782b6ac705ce394c16ff98
Natady25 <nata.dwiyanti@gmail.com>
2026-05-03 11:16:19 +0900

    Update 14_1.14.py

--- Chapter_01/14_1.14.py
+++ Chapter_01/14_1.14.py
@@ -1,3 +1,4 @@
+# Versi 1
 import turtle
 turtle.showturtle()
 
@@ -11,4 +12,26 @@ turtle.right(120)
 turtle.forward(100)
 turtle.right(120)
 turtle.forward(100)
+turtle.done()
+
+# Versi 2
+import turtle
+turtle.showturtle()
+
+turtle.right(60)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+
+# Versi 3
+import turtle
+turtle.showturtle()
+
+turtle.right(60)
+for i in range(3):
+  turtle.forward(100)
+  turtle.right(120)
+turtle.done()
 turtle.done()
