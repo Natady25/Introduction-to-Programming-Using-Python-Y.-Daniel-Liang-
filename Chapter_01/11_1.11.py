72c72d4faf987fd843451c6f692ddce861acf27a
Natady25 <nata.dwiyanti@gmail.com>
2026-06-09 18:54:56 +0900

    Update 11_1.11.py

--- Chapter_01/11_1.11.py
+++ Chapter_01/11_1.11.py
@@ -1,27 +1,23 @@
 # Versi 1
-birth_in_7_sec = 1
-birth_per_hour = 3600 // 7
-birth_per_day = 24 * birth_per_hour
-
-immigrants_in_45_sec = 1
-immigrants_per_hour = 3600 // 45
-immigrants_per_day = 24 * immigrants_per_hour
-
-deaths_in_13_sec = 1
-deaths_per_hour = 3600 // 13
-deaths_per_day = 24 * deaths_per_hour
+SECONDS_IN_YEAR = 365 * 24 * 60 *60
+BIRTH_RATE = 7
+DEATH_RATE = 13
+IMMIGRANT_RATE = 45
 
 current_population = 312032486
-add_pop_per_day = birth_per_day + immigrants_per_day - deaths_per_day
-add_pop_per_year = 365 * add_pop_per_day
+
+births_per_year = SECONDS_IN_YEAR // BIRTH_RATE
+deaths_per_year = SECONDS_IN_YEAR // DEATH_RATE
+immigrants_per_year = SECONDS_IN_YEAR // IMMIGRANT_RATE
+
+add_pop_per_year = births_per_year + immigrants_per_year - deaths_per_year
 
 years = 1
-a = current_population + add_pop_per_year
 while years < 6:
+	current_population = current_population + add_pop_per_year
 
-	print("Total population in year", years, "is =", a)
+	print("Total population in year", years, "is =", current_population)
 	years += 1
-	a += add_pop_per_year
 
 # Versi 2
 population = 312032486
